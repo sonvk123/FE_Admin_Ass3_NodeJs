ceb904242f4b6b25cc7b8f114fbb70b43ee87b16
sonvk123 <70042474+sonvk123@users.noreply.github.com>
2024-02-19 15:51:26 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Chat from "./Chat/Chat";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
